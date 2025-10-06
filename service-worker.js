7c185f615a24a8281bbdc207afb0340530bfbee4
Furgersen7 <jbwuf@aim.com>
2025-10-05 18:12:04 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,38 @@
-const CACHE_NAME = "f7beats-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+const CACHE_NAME = 'f7beats-cache-v2';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
-// Install: cache files
-self.addEventListener("install", event => {
+// Install & pre-cache
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(URLS_TO_CACHE))
+      .then(self.skipWaiting())
   );
 });
 
-// Fetch: serve cached content first
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
-});
-
-// Activate: clear old caches
-self.addEventListener("activate", event => {
+// Activate & remove old caches
+self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
     )
+  );
+  self.clients.claim();
+});
+
+// Network-first fetch
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
